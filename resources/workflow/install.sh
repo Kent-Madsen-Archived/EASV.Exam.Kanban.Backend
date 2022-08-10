c1cfb514f23c7db36c3ea38fbb9d376de48d714d
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-10 21:58:04 +0200

    change

--- resources/workflow/install.sh
+++ resources/workflow/install.sh
@@ -5,6 +5,7 @@ cd code
 echo "installation script"
 echo "installing php libraries"
 composer install -q --no-ansi --no-interaction --no-scripts --no-progress
+composer update
 
 echo "generating key"
 php artisan key:generate
