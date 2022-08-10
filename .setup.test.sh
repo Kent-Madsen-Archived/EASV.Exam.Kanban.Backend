5da310e286c990eb8362e8214319334b5719d940
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-10 17:56:28 +0200

    working on workflow

--- .setup.test.sh
+++ .setup.test.sh
@@ -4,7 +4,4 @@ bash ./resources/workflow/setup.sh
 
 bash ./resources/workflow/install.sh
 
-cd code
-
-echo "$PWD"
-php artisan test
+bash ./resources/workflow/permission.sh
--- resources/workflow/permission.sh
+++ resources/workflow/permission.sh
@@ -1,1 +1,4 @@
 #!/bin/env/bash
+cd code
+
+chmod -R 777 storage bootstrap/cache
