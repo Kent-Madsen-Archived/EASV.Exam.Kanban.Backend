90b0ded1812ea3b3544bc257dafcbcbd172d6724
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-12 18:18:49 +0200

    added task object

---
+++ code/app/Http/Controllers/Task.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskModel;
+use App\Http\Requests\StoreTaskModelRequest;
+use App\Http\Requests\UpdateTaskModelRequest;
+
+class Task extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTaskModelRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTaskModelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TaskModel  $taskModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskModel $taskModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTaskModelRequest  $request
+     * @param  \App\Models\TaskModel  $taskModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTaskModelRequest $request, TaskModel $taskModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TaskModel  $taskModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskModel $taskModel)
+    {
+        //
+    }
+}
