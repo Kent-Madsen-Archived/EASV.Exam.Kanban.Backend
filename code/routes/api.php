4b43a53be517c1d0a1b63034ff9016d5880644cf
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-13 09:23:57 +0200

    c

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,14 +1,7 @@
 <?php
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
+    const middlewareSanctum = 'auth:sanctum';
 
 
-    Route::middleware( 'auth:sanctum' )->get( '/user',
-        function( Request $request )
-        {
-            return $request->user();
-        }
-    );
 
 
 ?>
