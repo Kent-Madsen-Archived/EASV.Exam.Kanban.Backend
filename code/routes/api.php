832e45fa4d93c77623ec80ade582ce539773adad
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-14 19:25:33 +0200

    added comments

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,13 +1,20 @@
 <?php
     const middlewareSanctum = 'auth:sanctum';
 
-
+    // account
     require_once 'api/accountAPI.php';
+
+    // Resources
     require_once 'api/imagesAPI.php';
+
+    // Application
     require_once 'api/projectAPI.php';
     require_once 'api/settingAPI.php';
+
     require_once 'api/taskAPI.php';
     require_once 'api/taskGroupAPI.php';
+
+    // Teams
     require_once 'api/teamMemberControllerAPI.php';
     require_once 'api/teamMemberOptionsControllerAPI.php';
 ?>
