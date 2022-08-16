1f26bca496c1de80dded54b53b39c1adfb3c86c1
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-16 10:44:51 +0200

    reformating

--- code/app/Models/ProjectModelConfigurationModel.php
+++ code/app/Models/ProjectModelConfigurationModel.php
@@ -1,11 +1,13 @@
 <?php
+    namespace App\Models;
 
-namespace App\Models;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class ProjectModelConfigurationModel extends Model
-{
-    use HasFactory;
-}
+    class ProjectModelConfigurationModel
+        extends Model
+    {
+        use HasFactory;
+    }
+?>
