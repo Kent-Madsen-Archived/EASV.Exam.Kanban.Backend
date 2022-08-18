abb64d1480f505f6c8fcdca528fc7cb17efec4ae
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-08-18 12:22:13 +0200

    working models

--- code/app/Models/ProjectTemplateModel.php
+++ code/app/Models/ProjectTemplateModel.php
@@ -35,7 +35,15 @@
 
         protected $casts =
         [
+            'id' => 'integer',
 
+            'title' => 'string',
+            'description' => 'string',
+
+            'implementation' => 'collection',
+
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime'
         ];
     }
 ?>
